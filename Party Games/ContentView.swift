1980aae8d6ee79a445a6eef4176321ab0fa57efd
Oliver Carlock <mocarlock@gmail.com>
2023-08-30 00:45:41 -0500

    Initial Commit

---
+++ Party Games/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Party Games
+//
+//  Created by Oliver Carlock on 8/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Party Games/Party_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Party_GamesApp.swift
+//  Party Games
+//
+//  Created by Oliver Carlock on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Party_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
